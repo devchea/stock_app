1c090c9eea0acce0b7e448dc42a7e0c34aee001a
devchea <rchea817@gmail.com>
2020-11-11 12:03:54 -0800

    "updates"

--- src/login.js
+++ src/login.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 
 const login = () => {
+    const [ user, setUser ] = useState('')
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [ passwordError, setPasswordError ] = useState('')
+    const [ hasAccount, sethasAccount ] = useState(false)
+    
     return (
         <div>
             <h1>login</h1>
