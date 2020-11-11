377f4624f12ec26c7068a418ab76d3f9a4cbb49c
devchea <rchea817@gmail.com>
2020-11-11 11:15:30 -0800

    "updates"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React from 'react'
 import './App.css';
+import Login from './login'
 
 function App() {
   return (
     <div className="Stock App">
       <header className="App-header">
       </header>
+      <Login/>
     </div>
   );
 }
